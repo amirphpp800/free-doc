4cb92217c6171e4bca3dcf129ca9e91c1013ad2d
amirphpp800 <hpopenai2024@gmail.com>
2025-12-13 22:08:35 +0330

    Update sampleData.js

--- assets/js/data/sampleData.js
+++ assets/js/data/sampleData.js
@@ -1,8 +1,10 @@
 import { article1 } from './articles/article-1.js';
 import { article2 } from './articles/article-2.js';
 import { article3 } from './articles/article-3.js';
+import { article4 } from './articles/article-4.js';
 
 export const articles = [
+    article4,
     article3,
     article2,
     article1
